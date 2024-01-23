a60f2abc5120d01acbfa4cb0b70d1f4fd6d3ed80
kwsni <44220303+kwsni@users.noreply.github.com>
2024-01-23 01:22:33 +0000

    Added tautulli script

---
+++ tautulli/throttle_qbt.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+'''
+Throttles qBittorrent traffic while Plex detects active playback
+
+Required python modules: requests
+
+Tautulli triggers: Playback start, Playback resume, Playback stop, Playback pause
+'''
+import sys, requests
+
+# Change these to match your configuration
+QBIT_HOSTNAME = 'http://localhost:8080'
+QBIT_USER = 'admin'
+QBIT_PASS = 'adminadmin'
+
+def main():
+    try:
+        trigger = sys.argv[1]
+    except:
+        print('Invalid argument')
+        return
+    
+    # authenticate to qBittorrent WebUI API
+    url = f'{QBIT_HOSTNAME}/api/v2/auth/login'
+    headers = {'Referer': QBIT_HOSTNAME}
+    auth = {'username': QBIT_USER, 'password': QBIT_PASS}
+
+    login = requests.post(url, headers=headers, data=auth, verify=False)
+
+    if(login.status_code == 200 and login.cookies):
+        # check toggle state
+        url = f'{QBIT_HOSTNAME}/api/v2/transfer/speedLimitsMode'
+
+        state = requests.get(url, cookies=login.cookies, verify=False)
+        is_throttled = bool(int(state.content))
+
+        if(is_throttled and trigger == 'stop'):
+
+            url = f'{QBIT_HOSTNAME}/api/v2/transfer/toggleSpeedLimitsMode'
+
+            requests.post(url, cookies=login.cookies, verify=False)
+
+        elif(not is_throttled and trigger == 'start'):
+
+            url = f'{QBIT_HOSTNAME}/api/v2/transfer/toggleSpeedLimitsMode'
+
+            requests.post(url, cookies=login.cookies, verify=False)
+
+        # logout and end session
+            
+        url = f'{QBIT_HOSTNAME}/api/v2/auth/logout'
+
+        requests.post(url, cookies=login.cookies, verify=False)
+    else:
+        print('Login failed')
+        return
+
+if __name__ == '__main__':
+    main()
